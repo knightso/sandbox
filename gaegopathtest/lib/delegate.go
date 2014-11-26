89de68fbbbcfb23538803e890ba2372e4af27c35
hogedigo <ikeda@knightso.co.jp>
2014-11-26 16:18:21 +0900

    added.

--- gaegopathtest/lib/delegate.go
+++ gaegopathtest/lib/delegate.go
@@ -5,6 +5,7 @@ import "syscall"
 import "fmt"
 
 func init() {
+	fmt.Println("init for normal go!")
 	delegate = func() string {
 		return fmt.Sprintf("Hello! pid:", syscall.Getpid())
 	}
--- gaegopathtest/lib/delegate_gae.go
+++ gaegopathtest/lib/delegate_gae.go
@@ -1,7 +1,12 @@
 // +build appengine
 package lib
 
+import (
+	"fmt"
+)
+
 func init() {
+	fmt.Println("init for gae!")
 	delegate = func() string {
 		return "Hello GAE!"
 	}
